54e66df1da092fc397b0862322d8a5b646f8c41e
Connor Escajeda <escajedacj@hendrix.edu>
2022-09-19 11:21:30 -0500

    Gyroscope testing

--- fruit_collector/lib/main.dart
+++ fruit_collector/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
 }
 
 // https://docs.flutter.dev/cookbook/navigation/navigation-basics
+
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
@@ -132,12 +134,69 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => const SecondScreen()),
+            MaterialPageRoute(builder: (context) => const GameScreen()),
           );
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  double x = 0, y = 0, z = 0;
+  String direction = "none";
+
+  @override
+  void initState() {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      print(event);
+
+      x = event.x;
+      y = event.y;
+      z = event.z;
+
+      //rough calculation, you can use
+      //advance formula to calculate the orentation
+      if (x > 0) {
+        direction = "back";
+      } else if (x < 0) {
+        direction = "forward";
+      } else if (y > 0) {
+        direction = "left";
+      } else if (y < 0) {
+        direction = "right";
+      }
+
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gyroscope Sensor in Flutter"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30),
+          child: Column(children: [
+            Text(
+              direction,
+              style: TextStyle(fontSize: 30),
+            )
+          ])),
     );
   }
 }
