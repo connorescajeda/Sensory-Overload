1aa3bc7d5ef9d6068cea6729e1b93187f790ff55
nardkeza <mutayombank@hendrix.edu>
2022-09-20 15:16:47 -0500

    Light mode and dark mode themes for the app

--- fruit_collector/lib/main.dart
+++ fruit_collector/lib/main.dart
@@ -26,7 +26,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -120,7 +121,44 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
+      //https://docs.flutter.dev/cookbook/design/drawer
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text('Settings'),
+            ),
+            ListTile(
+              title: const Text('Light Mode'),
+              onTap: (){
+
+                Navigator.pop(context);
+              } ,
+            ),
+
+            ListTile(
+              title: const Text('Dark Mode'),
+              onTap: (){
+                theme: ThemeData(
+                  primarySwatch: Colors.grey,
+                  primaryColor: Colors.blue,
+                  brightness: Brightness.dark,
+
+                );
+                 
+                Navigator.pop(context);
+              },
+            ),
+          ],
+
+
+        ),
+      ),
     );
   }
 }
@@ -143,6 +181,55 @@ class _GameScreenState extends State<GameScreen> {
   List<double>? _gyroscopeValues;
   List<double>? _magnetometerValues;
 
+      x = event.x;
+      y = event.y;
+      z = event.z;
+
+      //rough calculation, you can use
+      //advance formula to calculate the orentation
+      if (x > 0.75) {
+        direction = "back";
+      } else if (x < -.75) {
+        direction = "forward";
+      } else if (y > 0) {
+        direction = "left";
+      } else if (y < 0) {
+        direction = "right";
+      }
+
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gyroscope Sensor in Flutter"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30),
+          child: Column(children: [
+            Text(
+              direction,
+              style: TextStyle(fontSize: 30),
+            )
+          ])),
+    );
+  }
+}
+
+class _GameScreen extends StatefulWidget {
+  const _GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => GameScreenState();
+}
+
+class GameScreenState extends State<GameScreen> {
   double x = 0, y = 0, z = 0;
   String direction = "none";
 
