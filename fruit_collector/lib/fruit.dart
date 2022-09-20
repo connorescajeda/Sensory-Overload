b57a198f94118d0351972da7c515f00abdd49a0e
Connor Escajeda <connor.escajeda@gmail.com>
2022-09-20 15:45:52 -0500

    Got fruits to appear on screen in a different color

--- fruit_collector/lib/fruit.dart
+++ fruit_collector/lib/fruit.dart
@@ -2,6 +2,15 @@ import 'dart:async';
 import 'dart:math' as math;
 
 class Fruit {
-  late math.Point<int> location;
   bool grabbed = false;
+  math.Point<int> location;
+  Fruit(this.location);
+
+  int get x {
+    return location.x;
+  }
+
+  int get y {
+    return location.y;
+  }
 }
